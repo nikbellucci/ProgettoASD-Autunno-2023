ef556b004494d2812273a70518e7ecdf4a239c4d
Niccolò Bellucci <niccolo.bellucci.dev@gmail.com>
2023-09-19 15:47:30 +0200

    Fixed third exercise

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -2,16 +2,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Esercizio3 {
     public static int numeroPromossi = 0;
-    public static ArrayList<Double> punteggio;
+    public static int numeroDipendenti = 0;
+    public static double[] punteggi;
     public static double threshold = 0;
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Utilizzo: java PremioPromozioneDipendenti <file_di_input>");
             return;
         }
-        punteggio = new ArrayList<>();
+        punteggi = new double[0];
         String inputFile = args[0];
         try {
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
@@ -19,13 +22,16 @@ public class Esercizio3 {
             while ((line = reader.readLine()) != null) {
                 if (line.contains("# numero di dipendenti")) {
                     String tmp = reader.readLine();
-                    numeroPromossi = Integer.parseInt(tmp);
+                    numeroDipendenti = Integer.parseInt(tmp);
                 }
                 if (line.contains("# vettore")) {
+                    int iteration = 0;
                     while (!line.contains("# soglia k")) {
                         line = reader.readLine();
                         if (line.matches("[0-9]*\\.?[0-9]+")) {
-                            punteggio.add(Double.parseDouble(line));
+                            punteggi = Arrays.copyOf(punteggi, iteration + 1);
+                            punteggi[iteration] = Double.parseDouble(line);
+                            iteration++;
                         } else {
                             threshold = Double.parseDouble(reader.readLine());
                             break;
@@ -34,33 +40,36 @@ public class Esercizio3 {
                 }
             }
             reader.close();
-            System.out.println(punteggio.size());
+            Arrays.sort(punteggi);
+            numeroPromossi = contaDipendentiPromossi(punteggi, threshold);
 
-            numeroPromossi = contaDipendentiPromossi(punteggio, threshold);
-
-             System.out.println("Numero totale di dipendenti promossi: " + numeroPromossi);
+             System.out.println("Numero totale di dipendenti premiati: " + numeroPromossi);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static int contaDipendentiPromossi(ArrayList<Double> indices, double k) {
-        return contaDipendentiPromossiDivideEtImpera(indices, k, 0, indices.size() - 1);
+    public static int contaDipendentiPromossi(double[] indices, double k) {
+        return contaDipendentiPromossiDivideEtImpera(indices, k, 0, indices.length - 1);
     }
 
-    public static int contaDipendentiPromossiDivideEtImpera(ArrayList<Double> indices, double k, int start, int end) {
+    public static int contaDipendentiPromossiDivideEtImpera(double[] indices, double k, int start, int end) {
         if (start > end) {
             return 0;
         }
 
         int middle = (start + end) / 2;
-        double midValue = indices.get(middle);
+        double midValue = indices[middle];
 
         if (midValue > k) {
+            // Il dipendente al centro supera la soglia k, quindi conta se stesso e
+            // le porzioni a sinistra e destra.
             return 1 + contaDipendentiPromossiDivideEtImpera(indices, k, start, middle - 1)
                     + contaDipendentiPromossiDivideEtImpera(indices, k, middle + 1, end);
         } else {
+            // Il dipendente al centro non supera la soglia k, quindi cerca solo a destra.
             return contaDipendentiPromossiDivideEtImpera(indices, k, middle + 1, end);
         }
     }
+
 }
