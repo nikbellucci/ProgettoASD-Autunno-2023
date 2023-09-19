93f6d32f431c656d7e2f4fb02d64eb29e3eca2ac
Niccolò Bellucci <niccolo.bellucci.dev@gmail.com>
2023-09-19 15:11:33 +0200

    Fixed bug on second exercise

--- src/Esercizio2.java
+++ src/Esercizio2.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Esercizio2 {
 
@@ -7,12 +8,11 @@ public class Esercizio2 {
         AVLTree tree = new AVLTree();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            String tmp = reader.readLine();
+            String line;
             int iteration = 0;
-            while (tmp != null) {
-                String[] arrSplit = tmp.split(" ");
+            while ((line = reader.readLine()) != null) {
+                String[] arrSplit = line.split(" ");
                 tree.insert(Integer.parseInt(arrSplit[0]), arrSplit[1]);
-                tmp = reader.readLine();
                 iteration++;
             }
         } catch (FileNotFoundException e) {
@@ -22,16 +22,58 @@ public class Esercizio2 {
         }
 
         // Visita inorder dell'albero
-        System.out.println("Visita inorder dell'albero:");
-        tree.inorderTraversal();
+        //System.out.println("Visita inorder dell'albero:");
+        //tree.inorderTraversal();
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Stampa tutte le chiavi in ordine crescente");
+            System.out.println("2. Stampa tutte le chiavi maggiori di una soglia");
+            System.out.println("3. Stampa delle coppie con a ≤ x ≤ b, e lunghezza della stringa ≤ s");
+            System.out.println("4. Esci");
+
+            System.out.print("Seleziona un'opzione: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Chiavi in ordine crescente:");
+                    tree.inorderTraversal();
+                    break;
+
+                case 2:
+                    System.out.print("Inserisci la soglia: ");
+                    int threshold = scanner.nextInt();
+                    System.out.println("Chiavi maggiori di " + threshold + ":");
+                    tree.printKeysGreaterThanThreshold(threshold);
+                    break;
+
+                case 3:
+                    System.out.print("Inserisci il valore di a: ");
+                    int a = scanner.nextInt();
+                    System.out.print("Inserisci il valore di b: ");
+                    int b = scanner.nextInt();
+                    System.out.print("Inserisci il valore di s: ");
+                    int s = scanner.nextInt();
+                    System.out.println("Coppie con a ≤ chiave ≤ b e lunghezza del valore ≤ s:");
+                    tree.printKeysInRangeAndStringLength(a, b, s);
+                    break;
+
+                case 4:
+                    scanner.close();
+                    System.exit(0);
+
+                default:
+                    System.out.println("Opzione non valida. Riprova.");
+            }
+        }
     }
-
 }
 
 class Node {
     protected int key;
-
     protected String value;
     protected int height;
     protected Node left;
@@ -151,5 +193,63 @@ class AVLTree {
     // Esegue una visita inorder dell'albero
     public void inorderTraversal() {
         inorderTraversal(root);
+    }
+
+    public void printKeysGreaterThanThreshold(Node node, int threshold) {
+        if (node == null) {
+            return;
+        }
+
+        // Effettua una visita inorder inversa, esplorando prima il sottoalbero destro.
+        printKeysGreaterThanThreshold(node.right, threshold);
+
+        // Stampa la chiave solo se è maggiore della soglia.
+        if (node.key > threshold) {
+            System.out.println("<" + node.key + ", " + node.value + ">");
+        }
+
+        // Continua l'esplorazione del sottoalbero sinistro.
+        printKeysGreaterThanThreshold(node.left, threshold);
+    }
+
+    public void printKeysGreaterThanThreshold(int threshold) {
+        printKeysGreaterThanThreshold(root, threshold);
+    }
+
+    public void printKeysInRangeAndStringLength(Node node, int a, int b, int s) {
+        if (node == null) {
+            return;
+        }
+
+        // Effettua una visita inorder inversa, esplorando prima il sottoalbero destro.
+        printKeysInRangeAndStringLength(node.right, a, b, s);
+
+        // Stampa la chiave e il valore solo se soddisfano i criteri specificati.
+        if (node.key >= a && node.key <= b && node.value.length() <= s) {
+            System.out.println("<" + node.key + ", " + node.value + ">");
+        }
+
+        // Continua l'esplorazione del sottoalbero sinistro.
+        printKeysInRangeAndStringLength(node.left, a, b, s);
+    }
+
+    public void printKeysInRangeAndStringLength(int a, int b, int s) {
+        printKeysInRangeAndStringLength(root, a, b, s);
+    }
+
+    public Node search(Node node, int key) {
+        if (node == null || node.key == key) {
+            return node;
+        }
+
+        if (key < node.key) {
+            return search(node.left, key);
+        } else {
+            return search(node.right, key);
+        }
+    }
+
+    public Node search(int key) {
+        return search(root, key);
     }
 }
